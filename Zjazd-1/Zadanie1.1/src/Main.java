08df3f58d80e326f6268d42648f19af8f77c092c
s36392 <s36392@pwsjtk.edu.pl>
2026-03-07 13:27:17 +0100

    Initial commit

--- Zjazd-1/Zadanie1.1/src/Main.java
+++ Zjazd-1/Zadanie1.1/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner myObj = new Scanner(System.in);
+        double a = myObj.nextDouble();
+        double b = myObj.nextDouble();
+        double c =
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        System.out.print()
+
+
         }
     }
 }
