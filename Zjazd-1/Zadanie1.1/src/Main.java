fce4a8519dc6ed1da490054e7861d2ce49cd8d06
grzegorz <s36392@pjwstk.edu.pl>
2026-03-18 19:56:25 +0100

    WIP

--- Zjazd-1/Zadanie1.1/src/Main.java
+++ Zjazd-1/Zadanie1.1/src/Main.java
@@ -2,13 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        double a = myObj.nextDouble();
-        double b = myObj.nextDouble();
-        double c =
-
-        System.out.print()
-
+        Scanner scanner
 
         }
     }
