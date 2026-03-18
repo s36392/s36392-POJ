7809f0b10c5c3edb277c5db93af24cb169f9a353
grzegorz <s36392@pjwstk.edu.pl>
2026-03-18 20:08:53 +0100

    Skonczone zadanie 1

--- Zjazd-1/Zadanie1.1/src/Main.java
+++ Zjazd-1/Zadanie1.1/src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a and b:");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        double area = (a * b)/2;
+
+        System.out.println("Area of a right triangle is: ");
+        System.out.println(area);
+
+        scanner.close();
         }
     }
-}
