3b3f8bfe1767255a52e6fa9a44953c655fe911d3
grzegorz <s36392@pjwstk.edu.pl>
2026-05-30 15:22:20 +0200

    Zadanie 6.4

---
+++ Zjazd-6/Zadanie-6.4/src/DemoRunners.java
@@ -0,0 +1,9 @@
+void main() {
+    Machine machine = new Machine();
+    Athlete athlete = new Athlete();
+    PoliticalCandidate politicalCandidate = new PoliticalCandidate();
+
+    machine.run();
+    athlete.run();
+    politicalCandidate.run();
+}
